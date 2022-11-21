6cb851b1172493d193fbac9ff792fa5543ad844b
Saverio Nasturzio <s.nasturzio@libero.it>
2022-11-21 18:50:36 +0100

    Made interpret_data.py more general, fixed fake flash access from application, and fixed makefile

--- heep_ultrasound/utils/interpret_data.py
+++ heep_ultrasound/utils/interpret_data.py
@@ -3,18 +3,22 @@ import struct
 import matplotlib.pyplot as plt
 import matplotlib
 
-if len(sys.argv) != 2:
+if len(sys.argv) not in [3, 2]:
     print("interpret_data.py filename")
     sys.exit(-1);
 
 filename = sys.argv[1]
 
+max_n = 35000
+if len(sys.argv) == 3:
+    max_n = int(sys.argv[2])
+
 res = []
 
 with open(filename, "rb") as file:
     while(word:=file.read(2)):
-        if word == b"\xff\xff":break
-        unpacked = struct.unpack(">H", word)
+        if len(res) >= max_n or word == b"\xff\xff":break
+        unpacked = struct.unpack("<H", word)
         res.append(unpacked[0])
 
 res = res[:len(res)]
